ad4feb04f04a39082fbaafe719549c4cfac9ea2f
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2018-03-12 00:03:39 -0400

    changed to make events repeat 30 times if a repetition is repeated

--- eventBotp1/src/main/java/eventBotp1/TimerTriggers.java
+++ eventBotp1/src/main/java/eventBotp1/TimerTriggers.java
@@ -1,6 +1,3 @@
-/*
-	want to eventually use timer.scheduleAtFixedRate(TimerTask, Date, long) to make the events fire on a repeated basis
-*/
 package eventBotp1;
 
 import java.time.Instant;
@@ -12,7 +9,7 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class TimerTriggers {
-	
+
 	// dms memb list
 	private List<Member> adds = eventBot.adds;
 	private Timer timer = new Timer();
@@ -34,50 +31,72 @@ public class TimerTriggers {
 		String repeats = s[0].trim(), eventName = s[1].trim(), eventDate = s[2].trim();
 		Long dateMills = 0L, dmFirstMills = 0L, dmSecondMills = 0L, dmThirdMills = 0L, chatFirstMills = 0L,
 				chatSecondMills = 0L, chatThirdMills = 0L;
+		eventDate = offSetCST(eventDate);
 		switch (repeats) {
 		case "yearly":
-			dmFirstMills = getEvent(eventDate, threeDay);
-			dmSecondMills = getEvent(eventDate, oneDay);
-			dmThirdMills = getEvent(eventDate, sixHour);
-			chatFirstMills = getEvent(eventDate, twoHour);
-			chatSecondMills = getEvent(eventDate, oneHour);
-			chatThirdMills = getEvent(eventDate, fifteenMin);
-			onTimerTriggers(dmFirstMills, threeDay, eventName);
-			onTimerTriggers(dmSecondMills, oneDay, eventName);
-			onTimerTriggers(dmThirdMills, sixHour, eventName);
-			onTimerTriggersGeneral(chatFirstMills, twoHour, eventName, msg);
-			onTimerTriggersGeneral(chatSecondMills, oneHour, eventName, msg);
-			onTimerTriggersGeneral(chatThirdMills, fifteenMin, eventName, msg);
-			Event(msg, eventName, eventDate);
+			for (int i = 0; i < 30; i++) {
+				dmFirstMills = getEvent(eventDate, threeDay);
+				dmSecondMills = getEvent(eventDate, oneDay);
+				dmThirdMills = getEvent(eventDate, sixHour);
+				chatFirstMills = getEvent(eventDate, twoHour);
+				chatSecondMills = getEvent(eventDate, oneHour);
+				chatThirdMills = getEvent(eventDate, fifteenMin);
+				onTimerTriggers(dmFirstMills, threeDay, eventName);
+				onTimerTriggers(dmSecondMills, oneDay, eventName);
+				onTimerTriggers(dmThirdMills, sixHour, eventName);
+				onTimerTriggersGeneral(chatFirstMills, twoHour, eventName, msg);
+				onTimerTriggersGeneral(chatSecondMills, oneHour, eventName, msg);
+				onTimerTriggersGeneral(chatThirdMills, fifteenMin, eventName, msg);
+				if (i < 1) {
+					Event(msg, eventName, eventDate);
+				}
+				eventDate = repeatEvent(eventDate, repeats);
+			}
 			break;
 		case "monthly":
-			dmFirstMills = getEvent(eventDate, twoDay);
-			dmSecondMills = getEvent(eventDate, sixHour);
-			chatFirstMills = getEvent(eventDate, oneHour);
-			chatSecondMills = getEvent(eventDate, fifteenMin);
-			onTimerTriggers(dmFirstMills, twoDay, eventName);
-			onTimerTriggers(dmSecondMills, sixHour, eventName);
-			onTimerTriggersGeneral(chatFirstMills, oneHour, eventName, msg);
-			onTimerTriggersGeneral(chatSecondMills, fifteenMin, eventName, msg);
-			Event(msg, eventName, eventDate);
+			for (int i = 0; i < 30; i++) {
+				dmFirstMills = getEvent(eventDate, twoDay);
+				dmSecondMills = getEvent(eventDate, sixHour);
+				chatFirstMills = getEvent(eventDate, oneHour);
+				chatSecondMills = getEvent(eventDate, fifteenMin);
+				onTimerTriggers(dmFirstMills, twoDay, eventName);
+				onTimerTriggers(dmSecondMills, sixHour, eventName);
+				onTimerTriggersGeneral(chatFirstMills, oneHour, eventName, msg);
+				onTimerTriggersGeneral(chatSecondMills, fifteenMin, eventName, msg);
+				if (i < 1) {
+					Event(msg, eventName, eventDate);
+				}
+				eventDate = repeatEvent(eventDate, repeats);
+			}
 			break;
 		case "weekly":
-			dmFirstMills = getEvent(eventDate, oneDay);
-			dmSecondMills = getEvent(eventDate, fourHour);
-			chatFirstMills = getEvent(eventDate, oneHour);
-			chatSecondMills = getEvent(eventDate, fifteenMin);
-			onTimerTriggers(dmFirstMills, oneDay, eventName);
-			onTimerTriggers(dmSecondMills, fourHour, eventName);
-			onTimerTriggersGeneral(chatFirstMills, oneHour, eventName, msg);
-			onTimerTriggersGeneral(chatSecondMills, fifteenMin, eventName, msg);
-			Event(msg, eventName, eventDate);
+			for (int i = 0; i < 30; i++) {
+				dmFirstMills = getEvent(eventDate, oneDay);
+				dmSecondMills = getEvent(eventDate, fourHour);
+				chatFirstMills = getEvent(eventDate, oneHour);
+				chatSecondMills = getEvent(eventDate, fifteenMin);
+				onTimerTriggers(dmFirstMills, oneDay, eventName);
+				onTimerTriggers(dmSecondMills, fourHour, eventName);
+				onTimerTriggersGeneral(chatFirstMills, oneHour, eventName, msg);
+				onTimerTriggersGeneral(chatSecondMills, fifteenMin, eventName, msg);
+				if (i < 1) {
+					Event(msg, eventName, eventDate);
+				}
+				eventDate = repeatEvent(eventDate, repeats);
+			}
 			break;
 		case "daily":
-			dmFirstMills = getEvent(eventDate, twoHour);
-			chatFirstMills = getEvent(eventDate, fifteenMin);
-			onTimerTriggers(dmFirstMills, twoHour, eventName);
-			onTimerTriggersGeneral(chatFirstMills, fifteenMin, eventName, msg);
-			Event(msg, eventName, eventDate);
+			for (int i = 0; i < 30; i++) {
+				dmFirstMills = getEvent(eventDate, twoHour);
+				chatFirstMills = getEvent(eventDate, fifteenMin);
+				onTimerTriggers(dmFirstMills, twoHour, eventName);
+				onTimerTriggersGeneral(chatFirstMills, fifteenMin, eventName, msg);
+				Event(msg, eventName, eventDate);
+				if (i < 1) {
+					Event(msg, eventName, eventDate);
+				}
+				eventDate = repeatEvent(eventDate, repeats);
+			}
 			break;
 		case "once":
 			dmFirstMills = getEvent(eventDate, twoHour);
@@ -91,10 +110,71 @@ public class TimerTriggers {
 		}
 	}
 
+	// change UTC to CST
+	public String offSetCST(String s) {
+		String[] time = s.split(" ");
+		String[] offSet = time[1].split(":");
+		int temp = Integer.valueOf(offSet[0]);
+		temp -= 5;
+		offSet[0] = String.valueOf(temp);
+		s = time[0] + " " + offSet[0] + ":" + offSet[1];
+		return s;
+	}
 
 	public void Event(MessageReceivedEvent event, String e, String f) {
 		event.getChannel().sendMessage(":fireworks: EVENT :fireworks:\n" + e + " is happening at:\n" + f
 				+ "\n+add to join the notificaiton list!").queue();
+	}
+
+	private String repeatEvent(String s, String re) {
+		String[] arr1 = s.split(" ");
+		String[] arr = arr1[0].split("/");
+		int temp = 0;
+		switch (re) {
+		case "yearly":
+			temp = Integer.valueOf(arr[2]);
+			temp++;
+			arr[2] = String.valueOf(temp);
+			s = "" + arr[0] + "/" + arr[1] + "/" + arr[2] + " " + arr1[1];
+			break;
+		case "monthly":
+			temp = Integer.valueOf(arr[0]);
+			temp++;
+			arr[0] = String.valueOf(temp);
+			s = "" + arr[0] + "/" + arr[1] + "/" + arr[2] + " " + arr1[1];
+			break;
+		case "weekly":
+			temp = Integer.valueOf(arr[1]);
+			if (temp < 32) {
+				temp += 7;
+				arr[1] = String.valueOf(temp);
+			} else {
+				temp -= temp + 7;
+				arr[1] = String.valueOf(temp);
+				temp = Integer.valueOf(arr[1]);
+				temp++;
+				arr[0] = String.valueOf(temp);
+			}
+			s = "" + arr[0] + "/" + arr[1] + "/" + arr[2] + " " + arr1[1];
+			break;
+		case "daily":
+			temp = Integer.valueOf(arr[2]);
+			if (temp < 32) {
+				temp++;
+				arr[1] = String.valueOf(temp);
+			} else {
+				temp = 1;
+				arr[1] = String.valueOf(temp);
+				temp = Integer.valueOf(arr[1]);
+				temp++;
+				arr[0] = String.valueOf(temp);
+			}
+			s = "" + arr[0] + "/" + arr[1] + "/" + arr[2] + " " + arr1[1];
+			break;
+		default:
+			return s;
+		}
+		return s;
 	}
 
 	private Long getEvent(String d, Long mills) {
@@ -135,7 +215,7 @@ public class TimerTriggers {
 				// iterator to get each member from list to dm them message arrow is for Java 8
 				// lambda
 				for (int i = 0; i < adds.size(); i++) {
-					//stop discord from blocking messages
+					// stop discord from blocking messages
 					if (i % 3 == 0) {
 						try {
 							Thread.sleep(1001);
