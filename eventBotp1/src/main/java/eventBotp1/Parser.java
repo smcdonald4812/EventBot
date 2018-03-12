aba5557ea7a2fb15858c83b2760c3b5c209e6b71
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2018-03-12 00:01:54 -0400

    added extra splits for time

--- eventBotp1/src/main/java/eventBotp1/Parser.java
+++ eventBotp1/src/main/java/eventBotp1/Parser.java
@@ -12,11 +12,16 @@ public class Parser extends ListenerAdapter{
 		if(args.contains("-")) {
 			args = args.replace("-", " ");
 		}
+		if(args.contains(":")) {
+			args = args.replace(":", " ");
+		}
 		//split into an array for proper checking takes M D Y and turns it into Y M D
 		String[] x = args.split(" ");
 		String a = x[0];
 		String b = x[1];
 		String c = x[2];
+		String d = x[3];
+		String e = x[4];
 		//instant must have YYYY-MM-DDTHH:MM:SSS
 		if(c.length() == 2) {
 			c = "20" + c;
@@ -27,8 +32,14 @@ public class Parser extends ListenerAdapter{
 		if(a.length() != 2) {
 			a = "0" + a;
 		}
+		if(d.length() != 2) {
+			d = "0" + d;
+		}
+		if(e.length() != 2) {
+			e = "0" + e;
+		}
 		//proper formatting for instant parsing (SE 8's new date time is a pain in the ass)
-		args = "" + c + "-" + a + "-" + b + "T" + x[3] + ":00Z";
+		args = "" + c + "-" + a + "-" + b + "T" + d + ":" + e + ":00Z";
 		return args;
 	}
 }
